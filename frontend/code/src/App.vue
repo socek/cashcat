a74c8db12d374e33479f1b9ab5f4b58fb0756872
Dominik Dlugajczyk <d.dlugajczyk@clearcode.cc>
2018-11-17 00:06:09 +0100

    Login page layeout fix.

--- frontend/code/src/App.vue
+++ frontend/code/src/App.vue
@@ -1,21 +1,15 @@
 <template>
   <div>
-    <b-navbar v-if="!isAuthenticated" toggleable="md" type="light" variant="light">
-      <b-navbar-brand href="#">Cashcat</b-navbar-brand>
-      <b-navbar-nav class="ml-auto">
-        <register v-if="!isAuthenticated"></register>
-      </b-navbar-nav>
-    </b-navbar>
 
-   <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow" v-if="isAuthenticated">
+   <nav class="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
      <a class="navbar-brand col-sm-3 col-md-1 mr-0" href="#/">Cashcat</a>
      <ul class="navbar-nav px-3">
-       <login></login>
+       <login v-if="isAuthenticated"></login>
+       <register v-if="!isAuthenticated"></register>
      </ul>
    </nav>
 
     <div class="container-fluid">
-      <router-view v-if="!isAuthenticated"></router-view>
       <nav class="col-md-1 d-none d-md-block bg-light sidebar" v-if="isAuthenticated">
         <div class="sidebar-sticky">
           <ul class="nav flex-column">
@@ -24,7 +18,7 @@
         </div>
       </nav>
 
-      <main id="main" role="main" class="col-md-10 ml-sm-auto col-lg-11">
+      <main role="main" class="col-md-10 ml-sm-auto" :class="{'col-lg-11': isAuthenticated, 'col-lg-12': !isAuthenticated}">
         <router-view></router-view>
       </main>
     </div>
